6f90da53fe0b4e063ce0a0d0792aedf34a6122b4
DinhVo <dinh.vo@mediastep.com>
2021-04-19 23:27:30 +0700

    feature user login,create

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,9 @@ const errorMiddleware=require('./middlewares/error')
 app.use(express.json())
 
 const products=require('./routes/product')
-
+const user=require('./routes/user')
 app.use('/api/v1',products)
-
+app.use('/api/v1',user)
 app.use(errorMiddleware)
 
 module.exports=app
---
+++ backend/controllers/userController.js
@@ -0,0 +1,19 @@
+const User=require('../models/user')
+const ErrorHandler=require('../utils/errorHandler')
+const catchAsyncErrors=require('../middlewares/catchAsyncError')
+exports.registerUser=catchAsyncErrors(async(req,res,next)=>{
+    const {name,email,password}=req.body
+    const user=await User.create({
+        name,
+        email,
+        password,
+        avatar:{
+            public_id:"2a274ebbb5879d870a69caae33d94388a88e0e35",
+            url:"https://static.remove.bg/remove-bg-web/2a274ebbb5879d870a69caae33d94388a88e0e35/assets/start_remove-79a4598a05a77ca999df1dcb434160994b6fde2c3e9101984fb1be0f16d0a74e.png"
+        }
+    })
+    res.status(201).json({
+        success:true,
+        user
+    })
+})
