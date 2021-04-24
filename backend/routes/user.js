363affffa40e09e06957522124a5ba2befa061a8
DinhVo <dinh.vo@mediastep.com>
2021-04-24 14:40:00 +0700

    recovery password

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,8 @@
 const express=require('express')
 const router=express.Router()
-const {registerUser,loginUser, logoutUser}=require('../controllers/userController')
+const {registerUser,loginUser, logoutUser, forgotPassword}=require('../controllers/userController')
 router.route('/user/create').post(registerUser)
 router.route('/user/login').post(loginUser)
 router.route('/user/logout').get(logoutUser)
+router.route('/user/password/forgot').post(forgotPassword)
 module.exports=router;
---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,21 @@
+const { options } = require("../routes/user");
+const nodemailer=require('nodemailer')
+const smtpTransport = require('nodemailer-smtp-transport');
+const sendEmail=async options=>{
+    var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'gmail',
+        host: 'smtp.gmail.com',
+        auth: {
+          user: 'kedinhvo2000@gmail.com',
+          pass: 'Dinhvo280700'
+        }
+      }));
+    const message={
+        from:`kedinhvo2000@gmail.com`,
+        to:options.email,
+        subject: options.subject,
+        text: options.text
+    }
+    await transporter.sendMail(message)
+}
+module.exports=sendEmail
