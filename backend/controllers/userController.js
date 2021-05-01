b3086ebcf2465e8a81c98388dc7acdd32dde3019
DinhVo <dinh.vo@mediastep.com>
2021-05-01 10:07:37 +0700

    commit gi cung ko bit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,16 +4,21 @@ const catchAsyncErrors=require('../middlewares/catchAsyncError')
 const sendToken=require('../utils/jwtToken')
 const sendEmail=require('../utils/sendEmail')
 const crypto=require('crypto')
-const { send } = require('process')
+const cloudinary=require('cloudinary')
 exports.registerUser=catchAsyncErrors(async(req,res,next)=>{
+    const result=await cloudinary.v2.uploader.upload(req.body.avatar,{
+        folder:'tazas',
+        width:150,
+        crop:'scale'
+    })
     const {name,email,password}=req.body
     const user=await User.create({
         name,
         email,
         password,
         avatar:{
-            public_id:"2a274ebbb5879d870a69caae33d94388a88e0e35",
-            url:"https://static.remove.bg/remove-bg-web/2a274ebbb5879d870a69caae33d94388a88e0e35/assets/start_remove-79a4598a05a77ca999df1dcb434160994b6fde2c3e9101984fb1be0f16d0a74e.png"
+            public_id:result.public_id,
+            url:result.secure_url
         }
     })
     sendToken(user,200,res)
