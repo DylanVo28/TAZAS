5c34f9142af843e5f0d41d4d598e796db12016bf
DinhVo <dinh.vo@mediastep.com>
2021-06-10 23:57:44 +0700

    remove product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,6 +7,7 @@ const cloudinary=require('cloudinary');
 const UserLogin = require('../models/userLogin');
 const User = require('../models/user');
 const Review = require('../models/review');
+const Inventory = require('../models/inventory');
 //add new product
 exports.newProduct=catchAsyncError (async (req,res,next)=>{
     if(!checkUrlImage(req.body.data.image)){
@@ -139,7 +140,8 @@ exports.deleteProduct=catchAsyncError(async(req,res,next)=>{
         return next(new ErrorHandler('Product not found',404));
     }
     await product.remove()
-    await Review.findByIdAndRemove({productId:req.params.id})
+    await Review.remove({productId:req.params.id})
+    await Inventory.remove({productId:req.params.id})
     res.status(200).json({
         success:true,
         message:"Product is deleted"
