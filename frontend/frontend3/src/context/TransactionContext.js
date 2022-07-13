e8904f01980e066ff79427588a8735ee0fb24cc3
DylanVo28 <kedinhvo2000@gmail.com>
2022-07-13 21:03:40 +0700

    handle transaction contract

--- frontend/frontend3/src/context/TransactionContext.js
+++ frontend/frontend3/src/context/TransactionContext.js
@@ -14,20 +14,30 @@ const getEthereumContract=()=>{
 
 
 
+
+
 export const TransactionProvider=({ children })=>{
     const [currentAccount,setCurrentAccount]=useState(null)
     const [formData,setFormData]=useState({address:'',amount:'',keyword:'',message:''})
+    const [contract,setContract]=useState(null)
+    useEffect(()=>{
+        getInfuraContract()
+    },[])
+    
     const handleChange=(e,name)=>{
         setFormData((prevState)=>({...prevState,[name]:e.target.value}))
     }
     const getInfuraContract=()=>{
-        const wsProvider=new ethers.providers.WebSocketProvider("wss://rinkeby.infura.io/ws/v3/54a57591f73c4c4696041b6d8d1460ea","rinkeby")
+        const wsProvider=new ethers.providers.WebSocketProvider("wss://mainnet.infura.io/ws/v3/54a57591f73c4c4696041b6d8d1460ea","rinkeby")
+       
+        const wallet = new ethers.Wallet('0c07df1d7dff826637d793112894610bd1081c6a989aa224466001d7fdbe311c', wsProvider)
       
+        const signer = wallet.provider.getSigner(wallet.address)
+        const contract=new ethers.Contract(contractAddress,contractABI, signer)
+            contract.on("sendData", (...args) => {
+                console.log(args);
+              });
 
-        const contract=new ethers.Contract(contractAddress,contractABI, wsProvider)
-        contract.on("*", (from, to, value, event) => {
-            console.log("event: ", event);
-          });
     
     }
     
@@ -78,17 +88,20 @@ export const TransactionProvider=({ children })=>{
             console.error(error)
         }
     }
-    const connectSmartContract=()=>{
-        getEthereumContract().CreateOrder("123456")
+    const connectSmartContract=async ()=>{
+        try{
+            getEthereumContract().CreateOrder("123456")
+
+        }
+        catch(e){
+            console.log(e)
+        }
 
         // console.log(getEthereumContract())
     }
 
   
-    useEffect(()=>{
-        // checkIfWalletIsConnected();
-        getInfuraContract()
-    },[])
+   
 return (
     <TransactionContext.Provider value={{connectWallet,currentAccount,formData,setFormData,handleChange,sendTransaction,connectSmartContract}}>
         {children}
